57c0ad4ca12e888b002d0572ed620fbf03b1dc84
Fred <domaine75@gmail.com>
2018-10-08 23:53:11 +0200

    Initial commit

---
+++ MathPgcd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MathPgcd
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("Searching for PGCD");
+      display($"PGCD(6, 9) is {Pgcd(6, 9)}");
+      display("Press any key to exit:");
+      Console.ReadLine();
+    }
+
+    public static int Pgcd(int a, int b)
+    {
+      if (b > a)
+      {
+        var tmp = a;
+        b = a;
+        a = tmp;
+      }
+
+      int r = a % b;
+      do
+      {
+        r = a % b;
+        a = b;
+        b = r;
+      }while(r != 0);
+      
+      return r;
+    }
+
+    public static (int, int) Swap(int a, int b)
+    {
+      return new ValueTuple<int, int>(b, a);
+    }
+  }
+}
---
+++ UnitTestPgcd/UnitTestPgcd.cs
@@ -0,0 +1,19 @@
+﻿using MathPgcd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPgcd
+{
+  [TestClass]
+  public class UnitTestPgcd
+  {
+    [TestMethod]
+    public void TestMethod_1()
+    {
+      const int source1 = 9;
+      const int source2 = 6;
+      const int expected = 3;
+      int result = Program.Pgcd(source1, source2);
+      Assert.AreEqual(result, expected);
+    }
+  }
+}
