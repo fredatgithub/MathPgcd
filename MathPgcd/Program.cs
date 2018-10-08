051f41b011d1d9b6283a64a29d3570f60a1e1712
Fred <domaine75@gmail.com>
2018-10-09 00:01:32 +0200

    add example

--- MathPgcd/Program.cs
+++ MathPgcd/Program.cs
@@ -9,6 +9,7 @@ namespace MathPgcd
       Action<string> display = Console.WriteLine;
       display("Searching for PGCD");
       display($"PGCD(6, 9) is {Pgcd(6, 9)}");
+      display($"PGCD(15, 5) is {Pgcd(5, 15)}");
       display("Press any key to exit:");
       Console.ReadLine();
     }
@@ -17,25 +18,31 @@ namespace MathPgcd
     {
       if (b > a)
       {
-        var tmp = a;
-        b = a;
-        a = tmp;
+        a = Math.Max(a, b);
+        b = Math.Min(a, b);
       }
 
       int r = a % b;
-      do
+      //do
+      //{
+      //  r = a % b;
+      //  a = b;
+      //  b = r;
+      //}while(r != 0);
+      
+      while (r != 0)
       {
         r = a % b;
         a = b;
         b = r;
-      }while(r != 0);
-      
-      return r;
+      }; 
+
+      return b;
     }
 
-    public static (int, int) Swap(int a, int b)
-    {
-      return new ValueTuple<int, int>(b, a);
-    }
+    //public static (int, int) Swap(int a, int b)
+    //{
+    //  return new ValueTuple<int, int>(b, a);
+    //}
   }
 }
